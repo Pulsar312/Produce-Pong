48ca7e1db23d9ae8931feba05187655fe05252d8
Nicholas <32116122+nicholasmy@users.noreply.github.com>
2022-03-13 20:41:29 -0400

    Refactor, change port, use env vars, enable unbuffered mode for printing

--- Dockerfile
+++ Dockerfile
@@ -11,14 +11,10 @@ COPY . .
 #add all dependencies from requirements file
 RUN pip3 install -r requirements.txt
 
-# Allow port 8080 to be accessed
-# from outside the container
-EXPOSE 8080
-
 # For the database, this helps it wait until database loads
 # Citation: This solution has been a reccomendation from Professor Jesse Hartloff, in his CSE 312 class at UB.
 ADD https://github.com/ufoscout/docker-compose-wait/releases/download/2.2.1/wait /wait
 RUN chmod +x /wait
 
 # Wait for database, and run the app
-CMD /wait && python3 app.py
+CMD /wait && python3 -u app.py
--- do_request.py
+++ do_request.py
@@ -3,27 +3,21 @@ import sys
 
 from flask import jsonify
 
-# method flushes print statements
-def flush():
-    sys.stdout.flush()
-    sys.stderr.flush()
-
 
 # method which signs user in (signs up if necessary)
 # returns the id of the user (or -1 if invalid)
 def sign_in(request, users, count_users):
     print(request)
-    flush()
-    data = json.loads(request.data)  #Note: request.get_json() doesn't word for some reason...
+    data = json.loads(request.data)  # Note: request.get_json() doesn't word for some reason...
 
     if users.count_documents({'username': data['username']}) == 0:  # username not in database, so create a new user
 
         # Get the next id
         user_id = -1
-        if count_users.count_documents({}) == 0: #if theres no users created yet, then the first id will be 1
+        if count_users.count_documents({}) == 0:  # if theres no users created yet, then the first id will be 1
             count_users.insert_one({"id_num": 1})
             user_id = 1
-        else: # otherwise, we have some users already, so we get the previous id, and update our database
+        else:  # otherwise, we have some users already, so we get the previous id, and update our database
             result = count_users.find_one({})
             previous_id = result["id_num"]
             new_value = {"$set": {"id_num": previous_id + 1}}
@@ -32,17 +26,13 @@ def sign_in(request, users, count_users):
 
         data['id'] = user_id  # set the id for the user
         users.insert_one(data)  # insert the user
-        result = users.find_one({'username': data['username'], 'password':data['password']})
-        print("User created with id:  ", result['id']) # just making sure it works
-        flush()
+        result = users.find_one({'username': data['username'], 'password': data['password']})
+        print("User created with id:  ", result['id'])  # just making sure it works
         return jsonify({'id': result['id']})  # returns the id to save as a cookie
-    elif(users.count_documents({'username': data['username'], 'password':data['password']}) == 1): # username and password exist in the database, so sign user in
+    elif users.count_documents({'username': data['username'], 'password': data['password']}) == 1:  # username and password exist in the database, so sign user in
         result = users.find_one({'username': data['username'], 'password': data['password']})
         print("User found with id:  ", result['id'])  # just making sure it works
-        flush()
         return jsonify({'id': result['id']})  # returns the id to save as a cookie
     else:  # username must be in database, but password doesn't match, so can't sign in
         print("Could not sign in, invalid username or password")
-        flush()
         return jsonify({'id': -1})  # returns the invalid id
-
