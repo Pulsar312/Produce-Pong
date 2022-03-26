73776fa729edc77e1e2841228948d1f411dacb2d
taisiago <taisiago@buffalo.edu>
2022-03-26 12:21:26 -0400

    Removed redundant parenthesis

--- food/Ingredient.py
+++ food/Ingredient.py
@@ -19,24 +19,24 @@ class Ingredient:
         if recipe not in self.extra_recipes:  # if we haven't added the recipe yet, then add it
             self.extra_recipes.append(recipe)
 
-    # method prints out all ingredients with the recipes
+    # method returns a string of all ingredients with their recipes
     def to_string(self):
-        str = "Name: " + self.name
+        ret_str = "Name: " + self.name
 
-        str += "\t\t\t\t\t"
-        str += "Main Recipes: ["
+        ret_str += "\t\t\t\t\t"
+        ret_str += "Main Recipes: ["
         for r in self.main_recipes:
-            str += r.name + ", "
-        if str[len(str) - 1] != "[":
-            str = str[:len(str) - 2]
-        str = str + "]"
+            ret_str += r.name + ", "
+        if ret_str[len(ret_str) - 1] != "[":
+            ret_str = ret_str[:len(ret_str) - 2]
+        ret_str = ret_str + "]"
 
-        str += "\t\t\t\t\t"
-        str += "Extra Recipes: ["
+        ret_str += "\t\t\t\t\t"
+        ret_str += "Extra Recipes: ["
         for r in self.extra_recipes:
-            str += r.name + ", "
-        if str[len(str) - 1] != "[":
-            str = str[:len(str) - 2]
-        str = str + "]"
+            ret_str += r.name + ", "
+        if ret_str[len(ret_str) - 1] != "[":
+            ret_str = ret_str[:len(ret_str) - 2]
+        ret_str = ret_str + "]"
 
-        return str
+        return ret_str
