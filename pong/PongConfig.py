936de83c44883cc1490de8716bc3658afb3ba7bc
taisiago <taisiago@buffalo.edu>
2022-04-26 14:18:55 -0400

    Ball and Paddle collision: ball velocity update.

--- pong/PongConfig.py
+++ pong/PongConfig.py
@@ -11,3 +11,5 @@ class PongConfig:
         self.game_height: int = 500
         self.game_width: int = 800
         self.increase_ball_speed_each_hit = False
+        self.max_y_speed = 300.0
+        self.speed_variation = 30
