63b5dd319ba4b4ddc14b3b9a599a78d2ddac73ae
taisiago <taisiago@buffalo.edu>
2022-05-10 15:28:20 -0400

    image path hardcoding removed for about page and replaced with templating

--- app.py
+++ app.py
@@ -43,13 +43,23 @@ def static(filename):
 @app.route("/about", methods=['GET'])
 def request_about():
     data = {"all_users": authentication.get_all_logged_in_users(), "len": len(authentication.get_all_logged_in_users())}
+
+    number_random_ingredients = 5
+    path: str = "./static/ingredients/"
+    ingredient_files: List[str] = [join(path, file) for file in listdir(path) if isfile(join(path, file))]
+    sample: List[str] = random.sample(ingredient_files, number_random_ingredients)
+    # data: Dict[str, str] = {}
+    for i in range(0, number_random_ingredients):
+        data["ingred" + str(i+1) + "_src"] = sample[i]
+
     return render_template("div_templates/about.html", **data)
 
 @app.route("/about_ingredients", methods=['GET'])
 def get_about_ingredients():
+    number_random_ingredients = 5
     path: str = "./static/ingredients/"
     ingredient_files: List[str] = [join(path, file) for file in listdir(path) if isfile(join(path, file))]
-    sample: List[str] = random.sample(ingredient_files, 5)
+    sample: List[str] = random.sample(ingredient_files, number_random_ingredients)
     return json.dumps(sample)
 
 #get the messages with the other user
